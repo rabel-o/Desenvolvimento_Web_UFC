fe5eb3328665bed6080590ded4e8956b1d9d862d
Gustavo Rabelo <106771316+rabel-o@users.noreply.github.com>
2024-03-15 18:00:52 -0300

    Add files via upload

---
+++ ativ_01/src/components/02MeuPC.jsx
@@ -0,0 +1,22 @@
+export const PlacaMae = ({ nome, preco }) => (
+    <div>
+        <h2>Placa Mãe: {nome}</h2>
+        <h3>Preço: R$ {preco}</h3>
+    </div>
+);
+
+
+export const Memoria = ({ nome, preco }) => (
+    <div>
+        <h2>Memoria: {nome}</h2>
+        <h3>Preço: R$ {preco}</h3>
+    </div>
+);
+
+
+export const PlacaDeVideo = ({ nome, preco }) => (
+    <div>
+        <h2>Placa de Video: {nome}</h2>
+        <h3>Preço: R$ {preco}</h3>
+    </div>
+);
